d0634b42461cbc02074c9ef9f0f824a13167fdd5
onehowon <flash0221@naver.com>
2024-09-05 21:47:17 +0900

    add: 프로젝트 설정

---
+++ src/main/java/com/ebiz/bcube/BcubeApplication.java
@@ -0,0 +1,13 @@
+package com.ebiz.bcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebiz/bcube/BcubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebiz.bcube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
